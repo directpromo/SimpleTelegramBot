7f09f98a1491e8d8f569a6d5fd9b0c2bb53dfb4f
Protocol Corporation <protocollive@outlook.com>
2021-04-10 19:07:46 -0300

    Added config variable types

--- src/config.php
+++ src/config.php
@@ -1,13 +1,14 @@
 <?php
-//2021.04.10.00
+//2021.04.10.01
 
+//(string) Token given by @BotFather
 const Token = '';
 
-//User ID to send log messages
-const DebugId = '';
+//(int) User ID to send log messages
+const DebugId = 0;
 
-//Language to use
+//(string) Language to use
 const DefaultLanguage = 'us';
 
-//Enable chat flow with attendants
+//(bool) Enable chat flow with attendants
 const ChatFlowEnable = true;
