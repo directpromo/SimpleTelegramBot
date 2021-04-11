1bfa8479f2ae3ba8ae9c1b747169f1e76bea1d19
Protocol Corporation <protocollive@outlook.com>
2021-04-11 15:25:15 -0300

    Command in bold

--- src/language/ptbr_chatflow.php
+++ src/language/ptbr_chatflow.php
@@ -1,5 +1,5 @@
 <?php
-//2021.04.11.00
+//2021.04.11.01
 
 const Lang_ChatFlow_Cmd_EndChat = '-sair';
 const Lang_ChatFlow_Cmd_Next = '-proximo';
@@ -10,5 +10,5 @@ const Lang_ChatFlow_ChattingWithAttendant = "Você está conversando com um aten
 const Lang_ChatFlow_NoAttenders = "Todos os nossos atendentes estão ocupados.\nContinuar aguardando?";
 const Lang_ChatFlow_DontWait = 'Ok. Espero te ver em breve.';
 const Lang_ChatFlow_ChatEnded = '<b>Sua conversa foi encerrada.</b>';
-const Lang_ChatFlow_UsersInWaitList = "Tem %s usuários na fila de atendimento.\nDiga %s para atender o róximo da fila.";
-const Lang_ChatFlow_ChattingWithClient = "Você está conversando com %s.\nSuas mensagens serão encaminhadas para ele.";
+const Lang_ChatFlow_UsersInWaitList = "Tem %s usuários na fila de atendimento.\nDiga <b>%s</b> para atender o róximo da fila.";
+const Lang_ChatFlow_ChattingWithClient = "Você está conversando com %s.\nSuas mensagens serão encaminhadas para ele.\nDiga <b>%s</b> para encerrar o chat.";
--- src/language/us_chatflow.php
+++ src/language/us_chatflow.php
@@ -1,5 +1,5 @@
 <?php
-//2021.04.11.00
+//2021.04.11.01
 
 const Lang_ChatFlow_Cmd_EndChat = '-exit';
 const Lang_ChatFlow_Cmd_Next = '-next';
@@ -10,5 +10,5 @@ const Lang_ChatFlow_ChattingWithAttendant = "You are chatting with an attendant.
 const Lang_ChatFlow_NoAttenders = "All our attendants are busy.\nKeep waiting?";
 const Lang_ChatFlow_DontWait = 'Ok. See you soon.';
 const Lang_ChatFlow_ChatEnded = '<b>Your chat are closed.</b>';
-const Lang_ChatFlow_UsersInWaitList = "Have %s users in the wait list.\nSay %s to take the next in line.";
-const Lang_ChatFlow_ChattingWithClient = "You are chatting with %s.\nYour messages will be forwarded to him.";
+const Lang_ChatFlow_UsersInWaitList = "Have %s users in the wait list.\nSay <b>%s</b> to take the next in line.";
+const Lang_ChatFlow_ChattingWithClient = "You are chatting with %s.\nYour messages will be forwarded to him.\Say <b>%s</b> to close the chat.";
